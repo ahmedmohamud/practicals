77ca0ed8322b1586840df48f0a4655c859e5110d
ahmedabdi <93647103+0710984396@users.noreply.github.com>
2022-01-27 12:32:02 +0300

    my commit

---
+++ JAVASCRIP.js/value.js
@@ -0,0 +1,8 @@
+use "git rm --cached <file>..." to unstage)
+        new file:   CODING QUESTIONS.JS
+        new file:   JAVASCRIP.js/value.js
+
+
+Student@DESKTOP-RV11AUR MINGW64 ~/Desktop/MY PRACTICE (master)
+$ git comit-m"���new javascript prac"
+git: 'comit-m���new javascript prac' is not a git command. See 'git --help'.
