5dbc017e62ed4ed05ad3ca007559d81469ead928
ahmedabdi <93647103+0710984396@users.noreply.github.com>
2022-02-01 12:15:44 +0300

    ��

---
+++ JAVASCRIP.js/strings.js
@@ -0,0 +1,76 @@
+var firstName = "ahmed"
+
+var secondName = "abdi"
+
+var thirdName = ""
+
+var fullName =firstName+" " + secondName+" " + thirdName
+console.log( fullName)
+
+// character access
+var myName = 'ahmed abdi'
+var charL= myName.charAt(1)
+console.log(charL);
+
+var myName = 'ahmed abdi'
+var charL= myName.charAt(6)
+console.log(charL);
+
+var myName = 'ahmed abdi'
+var lastindex = myName.length-1
+console.log(myName.charAt);
+
+
+
+var myName = 'ahmed abdi'
+var charL= myName.length-1
+console.log(charLlastindex)name.length-1)
+
+// character access 
+var myName = 'ahmed abdi'
+indexOf(myName)-value of post of charAt
+
+console.log(myName.charAt(indexOf); //u
+
+let myName = "ahmed "
+console.log(myName.toUpperCase())
+
+let myName = "ahmed "
+console.log(myName.toLocaleUpperCase())
+
+let myName = "ahmed "
+console.log(myName.toLocaleLowerCase())
+
+// split()
+let message = "30 weeks of code"
+console.log(message.split(' '));
+
+let message1 = "30 weeks of code"
+console.log(message1.split(' '));
+
+let message2 = "30 weeks of code"
+console.log(message2.split('  '));
+
+let myName = "ahmed, abdi "
+console.log(myName.split(','));
+
+let myName = "ahmed .abdi "
+console.log(myName.split(' . '));
+let msg = 'we love programming!'
+let msg = 'we love programming!'
+console.log(msg.split)();
+
+let myName = "ahmed"
+
+
+
+
+
+
+
+
+
+
+
+
+ 
--- JAVASCRIP.js/value.js
+++ JAVASCRIP.js/value.js
@@ -1,8 +1,61 @@
-use "git rm --cached <file>..." to unstage)
-        new file:   CODING QUESTIONS.JS
-        new file:   JAVASCRIP.js/value.js
+// // strings
+// var myName = 'ahmed'
+// console.log(myName);
 
 
-Student@DESKTOP-RV11AUR MINGW64 ~/Desktop/MY PRACTICE (master)
-$ git comit-m"���new javascript prac"
-git: 'comit-m���new javascript prac' is not a git command. See 'git --help'.
+// // variable.variableName.=. defination
+
+// let myAge = 22
+// // in i year i will be 23
+// let myAgeIn2023=myAge+1
+// console.log(myAgeIn2023);
+
+// let chairs = 200
+// let computers = 400
+// let books = 600
+// let total = chairs + computers + books 
+
+// console.log(total);
+
+// // booleans
+// var No = 'No'
+// var no = 'no'
+// if (No == No){
+//     console.log(true);
+// }
+// var computers = 4 > 3
+// console.log(false);
+
+// var myName = 'ahmed'
+// console.log(myName.toUpperCase());
+
+let myFirstName = 'ahmed'
+let mySecondName = 'abdi'
+let myFullName = myFirstName.concat(mySecondName)
+console.log(myFullName)
+
+let myFirstName = 'ahmed  '
+let mySecondName = 'abdi'
+let myFullName = myFirstName.concat(mySecondName)
+console.log(myFullName)
+// includes
+let mymsg = 'Hello world, I am ahmed!';
+console.log(mymsg.includes('ahmed '));
+
+let mymsg1 = 'Hello world, I am ahmed!';
+console.log(mymsg1.replace('ahmed','abdi'));
+
+let mymsg1 = 'Hello world, I am ahmed!';
+console.log(mymsg1.charCodeAt('mymsg1.indexOf(b'));
+
+let myName = 'ahmed'
+console.log(myName.startsWith('ah'));
+let myName = 'ahmed'
+console.log(myName.endsWith('d'));
+
+
+
+
+
+
+
